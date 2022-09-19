544d7d9b8f09f15148405600e5499b32563397f9
API Doc Deploy <fluid-api-deploy@fluidsynth.github.io>
2022-09-19 16:46:57 +0000

    Updating API doc from fluidsynth master

--- api/search/pages_3.js
+++ api/search/pages_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['fast_20file_20renderer_20for_20non_2drealtime_20midi_20file_20rendering_1373',['Fast file renderer for non-realtime MIDI file rendering',['../FileRenderer.html',1,'UsageGuide']]],
-  ['fluidsynth_202_2e2_20developer_20documentation_1374',['FluidSynth 2.2 Developer Documentation',['../index.html',1,'']]]
+  ['fluidsynth_202_2e3_20developer_20documentation_1374',['FluidSynth 2.3 Developer Documentation',['../index.html',1,'']]]
 ];
